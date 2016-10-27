4c6aa2971cbf6b8dc9851d003706e6ddfb9a5a0f
Jasonjys <jasonjys0511@gmail.com>
2016-10-27 12:54:22 -0400

    updated application template

--- app/pods/application/route.js
+++ app/pods/application/route.js
@@ -6,7 +6,7 @@ export default Ember.Route.extend({
   actions: {
     signIn: function(provider) {
       this.get('session').open('firebase', { provider: provider}).then(function(data) {
-        console.log(data.currentUser);
+        //console.log(data.currentUser);
         console.log(data.currentUser.displayName);
         //this.transitionTo('tasks');
       });
