0f72dc8e37b875e23a4c69ec5a8ecfc76b04cf1f
Jasonjys <jasonjys0511@gmail.com>
2016-10-28 12:15:28 -0400

    clean up

--- app/pods/application/route.js
+++ app/pods/application/route.js
@@ -13,24 +13,24 @@ export default Ember.Route.extend({
         // console.log(data.currentUser.email);
         // console.log(data.currentUser.displayName);
         //username = username.replace(/[^a-zA-Z0-9 -]/g, '');
-        const store = this.get('store');
-        return store.find('user', username).then(function(user) {
-          debugger;
-          return;
-        }, function() {
-          debugger;
-          // HACK: `find()` creates an entry in store.typeMapFor().idToRecord which prevents `createRecord()` from working
-          delete store.typeMapFor(store.modelFor('user')).idToRecord[username];
-          // A user couldn't be found, so create a new user
-          var user = store.createRecord('user', {
-            username: username,
-            provider: provider,
-            uid: uid,
-            tasks: null
-          });
-          // Save the user
-          user.save();
-        });
+        // const store = this.get('store');
+        // return store.find('user', username).then(function(user) {
+        //   debugger;
+        //   return;
+        // }, function() {
+        //   debugger;
+        //   // HACK: `find()` creates an entry in store.typeMapFor().idToRecord which prevents `createRecord()` from working
+        //   delete store.typeMapFor(store.modelFor('user')).idToRecord[username];
+        //   // A user couldn't be found, so create a new user
+        //   var user = store.createRecord('user', {
+        //     username: username,
+        //     provider: provider,
+        //     uid: uid,
+        //     tasks: null
+        //   });
+        //   // Save the user
+        //   user.save();
+        // });
       });
     },
     signOut: function() {
--- app/pods/tasks/route.js
+++ app/pods/tasks/route.js
@@ -3,21 +3,17 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model () {
     if (this.get('session.isAuthenticated')) {
-      let email = this.get('session.currentUser.email');
-      console.log(email);
-      console.log("filtered tasks: ",this.get('store').query('task', {
-        equalTo: this.get('session.currentUser.email')
-      }));
+      const email = this.get('session.currentUser.email');
       return this.get('store').query('task', {
         orderBy: 'email',
-        equalTo: this.get('session.currentUser.email')
+        equalTo: email
       });
     }
   },
   actions:{
     save: function() {
       var controller = this.get('controller'),
-        task = controller.get('model');
+      task = controller.get('model');
 
       return task.save();
     },
