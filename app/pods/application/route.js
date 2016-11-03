47958ddf5395d46b82ba403f351e260c90d25ca5
Jasonjys <jasonjys0511@gmail.com>
2016-11-03 00:28:39 -0400

    fixed login

--- app/pods/application/route.js
+++ app/pods/application/route.js
@@ -1,10 +1,10 @@
 import Ember from 'ember';
 export default Ember.Route.extend({
   beforeModel() {
-    if (!this.get('session.isAuthenticated')){
-      console.log('in application beforeModel and if');
-      this.transitionTo('login');
-    }
+    // if (!this.get('session.isAuthenticated')){
+    //   console.log('in application beforeModel and if');
+    //   this.transitionTo('login');
+    // }
     return this.get('session').fetch().catch(function() {});
-  },
+  }
 });
