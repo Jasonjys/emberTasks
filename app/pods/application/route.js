d73ffa5e575e6360edd7623de8070907b64725a9
Jasonjys <jasonjys0511@gmail.com>
2016-11-03 00:01:11 -0400

    fixed the problem of user will be loged out when they refresh the page

--- app/pods/application/route.js
+++ app/pods/application/route.js
@@ -1,6 +1,10 @@
 import Ember from 'ember';
 export default Ember.Route.extend({
   beforeModel() {
+    return this.get('session').fetch().catch(function() {});
+  },
+  
+  model() {
     if (!this.get('session.isAuthenticated')){
       console.log('in application beforeModel and if');
       this.transitionTo('login');
