cd58be48e94d08fcc62589f67ec5dc1615728948
Jasonjys <jasonjys0511@gmail.com>
2016-11-02 20:19:25 -0400

    handle login action in /login route

--- app/pods/application/route.js
+++ app/pods/application/route.js
@@ -2,39 +2,9 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 
   beforeModel () {
-    //this.transitionTo('tasks');
     return this.get('session').fetch().catch(function() {});
   },
   actions: {
-    signIn: function(provider) {
-      this.get('session').open('firebase', { provider: provider}).then((data) => {
-        // const username = data.currentUser.email;
-        // const uid = data.uid
-        // console.log(data.currentUser);
-        console.log(data.currentUser.email);
-        // console.log(data.currentUser.displayName);
-        //username = username.replace(/[^a-zA-Z0-9 -]/g, '');
-        // const store = this.get('store');
-        // return store.find('user', username).then(function(user) {
-        //   debugger;
-        //   return;
-        // }, function() {
-        //   debugger;
-        //   // HACK: `find()` creates an entry in store.typeMapFor().idToRecord which prevents `createRecord()` from working
-        //   delete store.typeMapFor(store.modelFor('user')).idToRecord[username];
-        //   // A user couldn't be found, so create a new user
-        //   var user = store.createRecord('user', {
-        //     username: username,
-        //     provider: provider,
-        //     uid: uid,
-        //     tasks: null
-        //   });
-        //   // Save the user
-        //   user.save();
-        // });
-        this.transitionTo('tasks');
-      });
-    },
     signOut: function() {
       console.log('in signOut function');
       this.transitionTo('login');
--- app/pods/login/route.js
+++ app/pods/login/route.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	beforeModel () {
+    return this.get('session').fetch().catch(function() {});
+  },
+  actions: {
+    signIn: function(provider) {
+      this.get('session').open('firebase', { provider: provider}).then((data) => {
+        console.log(data.currentUser);
+        console.log(data.currentUser.email);
+        this.transitionTo('tasks');
+      });
+    },
+    signOut: function() {
+      console.log('in signOut function');
+      this.transitionTo('login');
+      this.get('session').close();
+    }
+  }
 });
