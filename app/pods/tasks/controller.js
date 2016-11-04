5226ff23065ff6145104ff651f88779ce5260e79
Jasonjys <jasonjys0511@gmail.com>
2016-11-04 10:47:06 -0400

    added google map

--- app/pods/tasks/controller.js
+++ app/pods/tasks/controller.js
@@ -1,7 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-
+  lat: 32.75494243654723,
+  lng: -86.8359375,
+  zoom: 4,
+  
   actions: {
     edit: function(task) {
       task.set('isEditing', true);
