f938ef70bdd53b023335e932e969ae1fe070583d
helloWorldCindy <yusindy@rocketmail.com>
2016-11-07 09:49:16 -0500

    work on markers in google map

--- app/pods/tasks/controller.js
+++ app/pods/tasks/controller.js
@@ -1,18 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  lat: 56.130366,
-  lng: -106.34677099999999,
-  zoom: 3,
-  
-  markers: Ember.A([
-    {
-      id: 'marker1',
-      lat: null,
-      lng: null,
-    }
-  ]),
-
+  // markers: Ember.A([
+  //   {
+  //     id: 'marker1',
+  //     lat: this.get('lat'),
+  //     lng: this.get('lng')
+  //   }
+  // ]),
   actions: {
     edit: function(task) {
       task.set('isEditing', true);
@@ -20,18 +15,6 @@ export default Ember.Controller.extend({
     save: function(task) {
       task.set('isEditing', false);
       return true;
-    },
-    didUpdatePlace: function(attr){
-      this.set('lat', attr.lat);
-      this.set('lng', attr.lng);
-      this.set('zoom', 17);
-
-      let markersArray = this.get('markers');
-      let marker = markersArray.objectAt(0);
-      marker.lat = attr.lat;
-      marker.lng = attr.lng;
-
-      markersArray.arrayContentDidChange(0);
     }
   }
 });
--- app/pods/tasks/new/controller.js
+++ app/pods/tasks/new/controller.js
@@ -2,6 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   //session: Ember.inject.service(),
+  lat: 56.130366,
+  lng: -106.34677099999999,
+  zoom: 9,
+  markers: Ember.A([
+    {
+      id: 'marker1',
+      lat: null,
+      lng: null,
+    }
+  ]),
 
   init: function() {
     this.set('task',  Ember.Object.create());
@@ -11,7 +21,7 @@ export default Ember.Controller.extend({
     console.log("date", this.get('task.date'));
     console.log("description", this.get('task.description'));
     var isValid = true;
-    ['task.title', 'task.date', 'task.description'].forEach(function(field) {
+    ['task.title', 'task.date', 'task.location','task.description'].forEach(function(field) {
       if (this.get(field) === '' || Ember.typeOf(this.get(field)) === 'undefined') {
         isValid = false;
       }
@@ -31,16 +41,36 @@ export default Ember.Controller.extend({
         var newTask = this.store.createRecord('task', {
           title: this.get('task.title'),
           date: this.get('task.date'),
+          location: this.get('task.location'),
           description: this.get('task.description'),
+          position: {
+            lat:this.get('lat'),
+            lng:this.get('lng')
+          },
           email: this.get('session.currentUser.email')
         });
+        console.log("in addTask", newTask.position);
         newTask.save();
         this.setProperties({
           'task.title': '',
           'task.date': '',
+          'task.location':'',
+          'task.position':'',
           'task.description': ''
         });
         this.transitionToRoute('tasks');
+    },
+    didUpdatePlace: function(attr){
+      this.set('lat', attr.lat);
+      this.set('lng', attr.lng);
+      this.set('zoom', 17);
+
+      let markersArray = this.get('markers');
+      let marker = markersArray.objectAt(0);
+      marker.lat = attr.lat;
+      marker.lng = attr.lng;
+
+      markersArray.arrayContentDidChange(0);
     }
   }
 });
