11a10681b95b27b1eeb194bb12ab84b4e1722b8b
Jasonjys <jasonjys0511@gmail.com>
2016-11-10 12:27:59 -0500

    clean up

--- app/pods/application/route.js
+++ app/pods/application/route.js
@@ -1,12 +1,5 @@
 import Ember from 'ember';
 export default Ember.Route.extend({
-  // beforeModel() {
-  //   // if (!this.get('session.isAuthenticated')){
-  //   //   console.log('in application beforeModel and if');
-  //   //   this.transitionTo('login');
-  //   // }
-  //   return this.get('session').fetch().catch(function() {});
-  // }
   beforeModel() {
   	return this.get('session').fetch()
   		.catch(() => undefined); // ignore empty sessions
--- app/pods/login/route.js
+++ app/pods/login/route.js
@@ -1,8 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  beforeModel() {
-    return this.get('session').fetch()
-      .catch(() => undefined); // ignore empty sessions
-  },
 });
