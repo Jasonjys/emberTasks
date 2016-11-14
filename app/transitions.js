5e58bb4f813570132299a4fe777d54b96070229d
helloWorldCindy <yusindy@rocketmail.com>
2016-11-14 12:25:06 -0500

    add animation to task and task.new route

--- app/transitions.js
+++ app/transitions.js
@@ -2,7 +2,12 @@ export default function(){
 	this.transition(
 		this.hasClass('task-edit'),
 		this.toValue(false),
-		this.use('toRight'),
-		this.reverse('toLeft')
+		this.use('crossFade'),
+		this.reverse('crossFade')
+	);
+	this.transition(
+		this.matchSelector('.main-container'),
+	    this.use('crossFade'),
+	    this.reverse('crossFade')
 	);
 };
