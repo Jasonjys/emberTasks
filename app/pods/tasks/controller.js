346a8d6a6eb183c6ccdaef11fd2176663444eea2
Jasonjys <jasonjys0511@gmail.com>
2016-11-15 13:23:42 -0500

    added search panel

--- app/pods/tasks/controller.js
+++ app/pods/tasks/controller.js
@@ -2,6 +2,15 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   firebaseApp: Ember.inject.service(),
+  searchTerm: '',
+  matchingTasks: Ember.computed('model.tasks.@each.title','searchTerm', 
+    function() {
+      var searchTerm = this.get('searchTerm').toLowerCase();
+      return this.get('model.tasks').filter(function(task) {
+        return task.get('title').toLowerCase().indexOf(searchTerm) !==
+        -1;
+    });
+  }),
 
   actions: {
     edit: function(task) {
