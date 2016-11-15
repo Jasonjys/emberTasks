f3dd79ee22d1c0b53a862e8e7079c43501490fdc
Jasonjys <jasonjys0511@gmail.com>
2016-11-14 22:06:39 -0500

    fixed syntax

--- app/transitions.js
+++ app/transitions.js
@@ -7,10 +7,10 @@ export default function() {
 	);
 	this.transition(
 		this.matchSelector('.new-container'),
-	  this.use('toRight', {duration: 300}),
+	  this.use('toRight', {duration: 300})
 	);
 	this.transition(
 		this.matchSelector('.application-container'),
-	  this.use('toRight'),
+	  this.use('toRight')
 	);
 }
